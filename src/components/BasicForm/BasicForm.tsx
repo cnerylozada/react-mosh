8c9c3d993088ee640be6802c0c0d4cff3ccdb56b
cnerylozada <cnerylozada@cnerylozadas-MacBook-Pro.local>
2023-12-23 10:42:57 -0500

    feat: select was added in basic form

--- src/components/BasicForm/BasicForm.tsx
+++ src/components/BasicForm/BasicForm.tsx
@@ -2,6 +2,12 @@ import { SubmitHandler, useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { BasicFormType, validations } from "./validations";
 
+const categories = [
+  { id: 1, value: "Groceries" },
+  { id: 2, value: "Utilities" },
+  { id: 3, value: "Entertainment" },
+];
+
 export const BasicForm = () => {
   const {
     register,
@@ -20,15 +26,42 @@ export const BasicForm = () => {
       <form onSubmit={handleSubmit(onSubmit)}>
         <div>
           <div>
-            <input type="text" {...register("name")} />
-            {errors.name && <div>{errors.name.message}</div>}
+            <label htmlFor="description">Description</label>
+            <div>
+              <input
+                id="description"
+                type="text"
+                {...register("description")}
+              />
+            </div>
+            {errors.description && <div>{errors.description.message}</div>}
           </div>
           <div>
-            <input
-              type="number"
-              {...register("age", { valueAsNumber: true })}
-            />
-            {errors.age && <div>{errors.age.message}</div>}
+            <label htmlFor="amount">Amount</label>
+            <div>
+              <input
+                id="amount"
+                type="number"
+                {...register("amount", { valueAsNumber: true })}
+              />
+            </div>
+            {errors.amount && <div>{errors.amount.message}</div>}
+          </div>
+          <div>
+            <label htmlFor="category">Category</label>
+            <div>
+              <select id="category" {...register("category")} defaultValue={""}>
+                <option value={""} disabled>
+                  Select
+                </option>
+                {categories.map((_) => (
+                  <option key={_.id} value={_.id}>
+                    {_.value}
+                  </option>
+                ))}
+              </select>
+              {errors.category && <div>{errors.category.message}</div>}
+            </div>
           </div>
         </div>
         <div>
--- src/components/BasicForm/validations.ts
+++ src/components/BasicForm/validations.ts
@@ -1,12 +1,12 @@
 import { z } from "zod";
 
 export const validations = z.object({
-  name: z.string().min(1, "Required"),
-  age: z
-    .number({ invalid_type_error: "Only integers" })
-    .int()
-    .min(18, "At least 18 years")
-    .max(35, "At most 35 years"),
+  description: z.string().min(1, "Required"),
+  amount: z
+    .number({ invalid_type_error: "Only numbers" })
+    .min(10, "At least 10 dollars")
+    .max(50, "At most 50 dollars"),
+  category: z.string().min(1, "Required"),
 });
 
 export type BasicFormType = z.infer<typeof validations>;
