581656d0776d5b27202dadce336d8d548fca947d
cnerylozada <cnerylozada@cnerylozadas-MacBook-Pro.local>
2023-12-23 13:03:50 -0500

    feat: basic test

--- src/components/BasicForm/BasicForm.spec.tsx
+++ src/components/BasicForm/BasicForm.spec.tsx
@@ -1,3 +1,11 @@
+import { render, screen } from "@testing-library/react";
+import { BasicForm } from "./BasicForm";
+
 describe("BasicForm component", () => {
-  it("should ...", () => {});
+  it("should save values in inputs", () => {
+    const mockSetProduct = vi.fn();
+    render(<BasicForm setProduct={mockSetProduct} />);
+    const formTitle = screen.getByText(/basic form/i);
+    expect(formTitle).toBeInTheDocument();
+  });
 });
